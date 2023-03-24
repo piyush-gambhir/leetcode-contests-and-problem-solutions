d1a2612cb0993c51c03fec81c9fbbe80fdf01439
Piyush Gambhir <90608533+Piyush-Gambhir@users.noreply.github.com>
2023-03-24 08:55:49 +0530

    Added Solutions

--- Java/1466. Reorder Routes to Make All Paths Lead to the City Zero.java
+++ Java/1466. Reorder Routes to Make All Paths Lead to the City Zero.java
@@ -6,6 +6,32 @@ import java.util.*;
 
 class Solution {
     public int minReorder(int n, int[][] connections) {
-
+        int ans = 0;
+        ArrayList<Integer>[] graph = new ArrayList[n];
+        for (int i = 0; i < n; i++) {
+            graph[i] = new ArrayList<>();
+        }
+        for (int[] connection : connections) {
+            graph[connection[0]].add(connection[1]);
+            graph[connection[1]].add(-connection[0]);
+        }
+        boolean[] visited = new boolean[n];
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(0);
+        visited[0] = true;
+        while (!queue.isEmpty()) {
+            int node = queue.poll();
+            for (int child : graph[node]) {
+                if (child > 0 && !visited[child]) {
+                    ans++;
+                    queue.add(child);
+                    visited[child] = true;
+                } else if (child < 0 && !visited[-child]) {
+                    queue.add(-child);
+                    visited[-child] = true;
+                }
+            }
+        }
+        return ans;
     }
 }
