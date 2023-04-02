6f33f3483ede10985946c42a9e4e314961003a82
Piyush-Gambhir <90608533+Piyush-Gambhir@users.noreply.github.com>
2023-04-02 15:51:08 +0530

    Added Solutions

---
+++ Leetcode Contests Solutions/Weekly Contests/Weekly Contest 339/2609. Find the Longest Balanced Substring of a Binary String.java
@@ -0,0 +1,10 @@
+/*
+Problem Link: https://leetcode.com/contest/weekly-contest-339/problems/find-the-longest-balanced-substring-of-a-binary-string/
+*/
+import java.util.*;
+
+class Solution {
+    public int findTheLongestBalancedSubstring(String s) {
+        
+    }
+}
