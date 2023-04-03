29a68cf15aafdbd0199bfc8f3e4e44560bc60740
Piyush-Gambhir <90608533+Piyush-Gambhir@users.noreply.github.com>
2023-04-03 18:51:44 +0530

    Added Solutions

--- Leetcode Problems Solutions/Java/435. Non-overlapping Intervals.java
+++ Leetcode Problems Solutions/Java/435. Non-overlapping Intervals.java
@@ -6,6 +6,16 @@ import java.util.*;
 
 class Solution {
     public int eraseOverlapIntervals(int[][] intervals) {
-
+        if (intervals.length == 0)
+            return 0;
+        Arrays.sort(intervals, (a, b) -> a[1] - b[1]);
+        int ans = 0, prev = 0;
+        for (int i = 1; i < intervals.length; i++) {
+            if (intervals[i][0] < intervals[prev][1])
+                ans++;
+            else
+                prev = i;
+        }
+        return ans;
     }
 }
