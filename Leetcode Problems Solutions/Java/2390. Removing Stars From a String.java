c50a0028039b5a8fb5f3760de104ef71d23f85a6
Piyush-Gambhir <90608533+Piyush-Gambhir@users.noreply.github.com>
2023-04-11 14:30:59 +0530

    Added Solutions

---
+++ Leetcode Problems Solutions/Java/2390. Removing Stars From a String.java
@@ -0,0 +1,25 @@
+/*
+Problem Link: https://leetcode.com/problems/remove-all-adjacent-duplicates-in-string/
+*/
+
+import java.util.*;
+
+class Solution {
+    public String removeStars(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) != '*') {
+                stack.push(s.charAt(i));
+            } else {
+                stack.pop();
+            }
+        }
+
+        StringBuilder result = new StringBuilder();
+        while (!stack.isEmpty()) {
+            result.append(stack.pop());
+        }
+        return result.reverse().toString();
+    }
+}
--- Leetcode Problems Solutions/Java/543. Diameter of Binary Tree.java
+++ Leetcode Problems Solutions/Java/543. Diameter of Binary Tree.java
@@ -21,7 +21,27 @@ Problem Link: https://leetcode.com/problems/diameter-of-binary-tree/
 import java.util.*;
 
 class Solution {
-    public int diameterOfBinaryTree(TreeNode root) {
 
+    private int height(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int leftHeight = height(root.left);
+        int rightHeight = height(root.right);
+
+        return Math.max(leftHeight, rightHeight) + 1;
+    }
+
+    public int diameterOfBinaryTree(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int leftHeight = height(root.left);
+        int rightHeight = height(root.right);
+        int rootDiameter = leftHeight + rightHeight;
+        int leftDiameter = diameterOfBinaryTree(root.left);
+        int rightDiameter = diameterOfBinaryTree(root.right);
+        return Math.max(rootDiameter, Math.max(leftDiameter, rightDiameter));
     }
 }
