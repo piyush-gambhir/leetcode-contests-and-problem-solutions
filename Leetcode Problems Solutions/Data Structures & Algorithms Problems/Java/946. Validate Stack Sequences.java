b7a2749fd813f3d54e8b5ac38f8c748b69b679b9
Piyush Gambhir <90608533+Piyush-Gambhir@users.noreply.github.com>
2023-04-13 09:06:51 +0530

    Added Solutions

---
+++ Leetcode Problems Solutions/Data Structures & Algorithms Problems/Java/946. Validate Stack Sequences.java
@@ -0,0 +1,20 @@
+/*
+Problem Link: https://leetcode.com/problems/validate-stack-sequences/
+*/
+
+import java.util.*;
+
+class Solution {
+    public boolean validateStackSequences(int[] pushed, int[] popped) {
+        Stack<Integer> stack = new Stack<>();
+        int i = 0;
+        for (int num : pushed) {
+            stack.push(num);
+            while (!stack.isEmpty() && stack.peek() == popped[i]) {
+                stack.pop();
+                i++;
+            }
+        }
+        return stack.isEmpty();
+    }
+}
