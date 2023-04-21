2c6227a05b23b191c5822e3d92da8ede864a8b76
Piyush-Gambhir <90608533+Piyush-Gambhir@users.noreply.github.com>
2023-04-21 16:26:45 +0530

    Added Solutions

---
+++ Leetcode Problems Solutions/879. Profitable Schemes.java
@@ -0,0 +1,27 @@
+/*
+Problem Link: https://leetcode.com/problems/profitable-schemes/
+*/
+
+import java.util.*;
+
+class Solution {
+    public int profitableSchemes(int n, int minProfit, int[] group, int[] profit) {
+        int[][] dp = new int[n + 1][minProfit + 1];
+        dp[0][0] = 1;
+        int mod = (int) 1e9 + 7;
+        for (int i = 0; i < group.length; i++) {
+            int g = group[i];
+            int p = profit[i];
+            for (int j = n; j >= g; j--) {
+                for (int k = minProfit; k >= 0; k--) {
+                    dp[j][k] = (dp[j][k] + dp[j - g][Math.max(0, k - p)]) % mod;
+                }
+            }
+        }
+        int res = 0;
+        for (int i = 0; i <= n; i++) {
+            res = (res + dp[i][minProfit]) % mod;
+        }
+        return res;
+    }
+}
