30fa050f2ed0f9091f827555a6601156903f642a
Piyush Gambhir <90608533+Piyush-Gambhir@users.noreply.github.com>
2023-06-11 16:05:40 +0530

    Added Solutions

---
+++ Leetcode Problems Solutions/1802. Maximum Value at a Given Index in a Bounded Array.java
@@ -0,0 +1,10 @@
+/*
+Problem Link: https://leetcode.com/problems/maximum-value-at-a-given-index-in-a-bounded-array/
+*/
+
+import java.util.*;     
+class Solution {
+    public int maxValue(int n, int index, int maxSum) {
+        
+    }
+}
