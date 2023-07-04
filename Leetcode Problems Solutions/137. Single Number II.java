bc40165c36481b373768dee59a68a628c88ac7c3
Piyush Gambhir <90608533+Piyush-Gambhir@users.noreply.github.com>
2023-07-04 15:57:08 +0000

    Added Solutions

---
+++ Leetcode Problems Solutions/137. Single Number II.java
@@ -0,0 +1,22 @@
+/*
+Problem Link: https://leetcode.com/problems/single-number-ii/
+*/
+
+import java.util.*;
+
+class Solution {
+    public int singleNumber(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(nums[i]))
+                map.put(nums[i], map.get(nums[i]) + 1);
+            else
+                map.put(nums[i], 1);
+        }
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            if (entry.getValue() == 1)
+                return entry.getKey();
+        }
+        return -1;
+    }
+}
