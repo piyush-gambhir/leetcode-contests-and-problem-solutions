f91e81b3168527e09fe8229736e84a887152cd55
Piyush Gambhir <90608533+Piyush-Gambhir@users.noreply.github.com>
2023-07-23 14:53:38 +0000

    Added Solutions

---
+++ Leetcode Problems Solutions/894. All Possible Full Binary Trees.java
@@ -0,0 +1,45 @@
+/*
+Problem Link: https://leetcode.com/problems/all-possible-full-binary-trees/
+*/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+import java.util.*;
+
+class Solution {
+    public List<TreeNode> allPossibleFBT(int n) {
+        List<TreeNode> list = new ArrayList<>();
+        if (n == 1) {
+            list.add(new TreeNode(0));
+            return list;
+        }
+        n--;
+        for (int i = 1; i < n; i += 2) {
+            List<TreeNode> left = allPossibleFBT(i);
+            List<TreeNode> right = allPossibleFBT(n - i);
+            for (TreeNode l : left) {
+                for (TreeNode r : right) {
+                    TreeNode root = new TreeNode(0);
+                    root.left = l;
+                    root.right = r;
+                    list.add(root);
+                }
+            }
+        }
+        return list;
+    }
+}
