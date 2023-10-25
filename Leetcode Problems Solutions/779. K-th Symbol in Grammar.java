f280940acd5910320545a58fb062b8f743efef23
Piyush Gambhir <90608533+Piyush-Gambhir@users.noreply.github.com>
2023-10-25 13:14:42 +0530

    Added Solutions

---
+++ Leetcode Problems Solutions/779. K-th Symbol in Grammar.java
@@ -0,0 +1,18 @@
+/*
+Problem Link: https://leetcode.com/problems/k-th-symbol-in-grammar/
+*/
+
+import java.util.*;
+
+class Solution {
+    public int kthGrammar(int n, int k) {
+        if (n == 1 && k == 1)
+            return 0;
+
+        int mid = (int) Math.pow(2, n - 1) / 2;
+        if (k <= mid)
+            return kthGrammar(n - 1, k);
+        else
+            return kthGrammar(n - 1, k - mid) == 0 ? 1 : 0;
+    }
+}
