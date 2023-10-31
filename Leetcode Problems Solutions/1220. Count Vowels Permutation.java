2d69c2c997b01d3c46110b88f3ab5ddd18f32d14
Piyush Gambhir <90608533+Piyush-Gambhir@users.noreply.github.com>
2023-10-31 09:21:58 +0530

    Added Solutions

---
+++ Leetcode Problems Solutions/1220. Count Vowels Permutation.java
@@ -0,0 +1,11 @@
+/*
+Problem Link: https://leetcode.com/problems/count-vowels-permutation/description/
+*/
+
+import java.util.*;
+
+class Solution {
+    public int countVowelPermutation(int n) {
+
+    }
+}
