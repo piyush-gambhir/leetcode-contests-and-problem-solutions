b87d81c90620f31c3647507fd99d02d7758fdc9d
Piyush Gambhir <90608533+Piyush-Gambhir@users.noreply.github.com>
2023-12-06 14:17:15 +0530

    Added Solutions

---
+++ Leetcode Problems Solutions/2871. Split Array Into Maximum Number of Subarrays.java
@@ -0,0 +1,12 @@
+
+/*
+Problem Link: https://leetcode.com/problems/split-array-into-maximum-number-of-subarrays/
+*/
+
+import java.util.*;
+
+class Solution {
+    public int maxSubarrays(int[] nums) {
+        
+    }
+}
