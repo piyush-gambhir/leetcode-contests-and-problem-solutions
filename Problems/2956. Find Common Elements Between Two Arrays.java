bea3690f0d9218caa94d25a3b5474dbbb4b81e77
Piyush Gambhir <90608533+Piyush-Gambhir@users.noreply.github.com>
2023-12-10 14:12:44 +0530

    Added Solutions

---
+++ Problems/2956. Find Common Elements Between Two Arrays.java
@@ -0,0 +1,39 @@
+/*
+Problem Link: https://leetcode.com/problems/find-common-elements-between-two-arrays/description/
+*/
+
+import java.util.*;
+
+class Solution {
+    public int[] findIntersectionValues(int[] nums1, int[] nums2) {
+        HashMap<Integer, Integer> map1 = new HashMap<>();
+        HashMap<Integer, Integer> map2 = new HashMap<>();
+
+        for (int i = 0; i < nums1.length; i++) {
+            map1.put(nums1[i], map1.getOrDefault(nums1[i], 0) + 1);
+        }
+
+        for (int i = 0; i < nums2.length; i++) {
+            map2.put(nums2[i], map2.getOrDefault(nums2[i], 0) + 1);
+        }
+
+        int[] result = new int[2];
+
+        for (int i = 0; i < nums1.length; i++) {
+            if (map2.containsKey(nums1[i])) {
+                result[0] += 1;
+            }
+
+        }
+
+        for (int i = 0; i < nums2.length; i++) {
+            if (map1.containsKey(nums2[i])) {
+                result[1] += 1;
+            }
+
+        }
+
+        return result;
+
+    }
+}
---
+++ Problems/2958. Length of Longest Subarray With at Most K Frequency.java
@@ -0,0 +1,33 @@
+/*
+Problem Links: https://leetcode.com/problems/length-of-longest-subarray-with-frequency-at-most-k/description/
+*/
+
+import java.util.*;
+
+class Solution {
+    public int maxSubarrayLength(int[] nums, int k) {
+        int i = 0;
+        int j = 0;
+        int n = nums.length;
+        Map<Integer, Integer> map = new HashMap<>();
+
+        int ans = 0;
+        while (j < n) {
+            map.put(nums[j], map.getOrDefault(nums[j], 0) + 1);
+            if (map.get(nums[j]) > k) {
+                while (i < j && map.get(nums[j]) > k) {
+                    if (map.get(nums[i]) == 1)
+                        map.remove(nums[i]);
+                    else
+                        map.put(nums[i], map.get(nums[i]) - 1);
+                    i++;
+                }
+            }
+            ans = Math.max(ans, j - i + 1);
+
+            j++;
+        }
+
+        return ans;
+    }
+}
