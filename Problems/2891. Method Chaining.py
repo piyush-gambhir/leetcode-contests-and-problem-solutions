74dcabef6605c0f6ff617c1a9c66b298a1dea710
Piyush Gambhir <90608533+Piyush-Gambhir@users.noreply.github.com>
2023-12-20 15:04:06 +0530

    Added Solutions

--- Problems/2891. Method Chaining.py
+++ Problems/2891. Method Chaining.py
@@ -2,8 +2,9 @@
 Problem Link: https://leetcode.com/problems/method-chaining/
 """
 
+
 import pandas as pd
 
 
 def findHeavyAnimals(animals: pd.DataFrame) -> pd.DataFrame:
-    return animals[animals.weight > 100][['name']].sort_values(by='weight', ascending=False)
+    return animals[animals.weight > 100].sort_values(by='weight', ascending=False)[['name']]
