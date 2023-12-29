8f53b7c6299b9f0d7be431935f5ee1671a87a92d
piyush-gambhir <90608533+piyush-gambhir@users.noreply.github.com>
2023-12-29 16:44:02 +0530

    Added Solutions

---
+++ Problems/1531. String Compression II.java
@@ -0,0 +1,12 @@
+/*
+Problem Link: https://leetcode.com/problems/string-compression-ii/description/
+*/
+
+import java.util.*;
+
+class Solution {
+    public int getLengthOfOptimalCompression(String s, int k) {
+
+        
+    }
+}
