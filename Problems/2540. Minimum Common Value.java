de239b64ee75ef17250f79100a899fef9d38865c
Piyush Gambhir <90608533+piyush-gambhir@users.noreply.github.com>
2024-03-09 12:21:59 +0530

    Added Solution For Problem 2540

---
+++ Problems/2540. Minimum Common Value.java
@@ -0,0 +1,21 @@
+/*
+Problem Link: https://leetcode.com/problems/minimum-common-value/description/
+*/
+
+import java.util.*;
+
+class Solution {
+    public int getCommon(int[] nums1, int[] nums2) {
+        int i = 0, j = 0;
+        while (i < nums1.length && j < nums2.length) {
+            if (nums1[i] == nums2[j]) {
+                return nums1[i];
+            } else if (nums1[i] < nums2[j]) {
+                i++;
+            } else {
+                j++;
+            }
+        }
+        return -1;
+    }
+}
