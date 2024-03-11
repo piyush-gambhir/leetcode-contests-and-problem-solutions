746f610e6afcf10b416648b52b3653df5a407974
piyush.gambhir <piyush.gambhir@spyne.ai>
2024-03-11 16:02:18 +0530

    Added Solutions For Problems 6, 791

---
+++ Problems/6. Zigzag Conversion.java
@@ -0,0 +1,11 @@
+/*
+Problem Link: https://leetcode.com/problems/zigzag-conversion
+*/
+
+import java.util.*;
+
+class Solution {
+    public String convert(String s, int numRows) {
+        
+    }
+}
--- Problems/791. Custom Sort String.java
+++ Problems/791. Custom Sort String.java
@@ -6,7 +6,25 @@ import java.util.*;
 
 class Solution {
     public String customSortString(String order, String s) {
-        
 
+        int[] count = new int[26];
+        for (char c : s.toCharArray()) {
+            count[c - 'a']++;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (char c : order.toCharArray()) {
+            while (count[c - 'a']-- > 0) {
+                sb.append(c);
+            }
+        }
+
+        for (int i = 0; i < 26; i++) {
+            while (count[i]-- > 0) {
+                sb.append((char) ('a' + i));
+            }
+        }
+
+        return sb.toString();
     }
 }
