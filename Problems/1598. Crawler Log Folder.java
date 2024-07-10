b9644b7e78d0744561d5d0692f19d0e47ab80b89
piyush-gambhir <90608533+piyush-gambhir@users.noreply.github.com>
2024-07-11 01:27:25 +0530

    feat: Add solution for Problem 1598. Crawler Log Folder

---
+++ Problems/1598. Crawler Log Folder.java
@@ -0,0 +1,21 @@
+/*
+Problem Link: https://leetcode.com/problems/crawler-log-folder/
+*/
+
+import java.util.*;
+
+class Solution {
+    public int minOperations(String[] logs) {
+        Stack<String> stack = new Stack<>();
+        for (String log : logs) {
+            if (log.equals("../")) {
+                if (!stack.isEmpty()) {
+                    stack.pop();
+                }
+            } else if (!log.equals("./")) {
+                stack.push(log);
+            }
+        }
+        return stack.size();
+    }
+}
